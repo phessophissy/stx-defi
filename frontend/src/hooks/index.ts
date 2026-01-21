c1427d6ad0f2ea47cd9cac306a27387b965437b0
phessophissy <adefesobi@yahoo.com>
2026-01-21 21:00:42 +0100

    feat: Update hooks barrel export with utility hooks

--- frontend/src/hooks/index.ts
+++ frontend/src/hooks/index.ts
@@ -2,3 +2,5 @@ export { useCorePool } from './useCorePool';
 export { useYieldVault } from './useYieldVault';
 export { useContractTransactions } from './useContractTransactions';
 export { useTransactionStatus } from './useTransactionStatus';
+export { useLocalStorage } from './useLocalStorage';
+export { useDebounce, useDebouncedCallback } from './useDebounce';
