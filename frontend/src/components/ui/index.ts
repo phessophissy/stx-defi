a6c8a8f07ddf8078875436b612b7dc911d7593de
phessophissy <adefesobi@yahoo.com>
2026-01-21 19:32:07 +0100

    feat: Add UI components barrel export

---
+++ frontend/src/components/ui/index.ts
@@ -0,0 +1,11 @@
+export { Button } from './Button';
+export type { ButtonProps } from './Button';
+
+export { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from './Card';
+
+export { Input } from './Input';
+export type { InputProps } from './Input';
+
+export { Modal } from './Modal';
+
+export { ToastProvider, useToast } from './Toast';
