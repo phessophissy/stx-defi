01b9a38d07f815b20a604ec20d359cce5b85f358
phessophissy <adefesobi@yahoo.com>
2026-01-22 23:25:50 +0100

    theme(54/60): Export InteractiveBall from index

--- frontend/src/components/ui/bouncing-balls/index.ts
+++ frontend/src/components/ui/bouncing-balls/index.ts
@@ -2,3 +2,4 @@ export { BouncingBall } from './BouncingBall';
 export { BouncingBallsDecor } from './BouncingBallsDecor';
 export { FloatingBall } from './FloatingBall';
 export { BackgroundBalls } from './BackgroundBalls';
+export { InteractiveBall } from './InteractiveBall';
