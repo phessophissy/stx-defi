ed4a101760f4ad14d96bff863ba95bc2bfb0b014
phessophissy <adefesobi@yahoo.com>
2026-01-22 23:05:24 +0100

    theme(34/60): Create BackgroundBalls wrapper component

---
+++ frontend/src/components/ui/bouncing-balls/BackgroundBalls.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { BouncingBallsDecor } from './BouncingBallsDecor';
+
+export function BackgroundBalls() {
+  return (
+    <>
+      <BouncingBallsDecor />
+    </>
+  );
+}
