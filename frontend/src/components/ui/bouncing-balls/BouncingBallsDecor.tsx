24374671c672317a04e61fe1068f5b2c6c83bc44
phessophissy <adefesobi@yahoo.com>
2026-01-22 23:19:02 +0100

    theme(45/60): Add position prop to BouncingBallsDecor

--- frontend/src/components/ui/bouncing-balls/BouncingBallsDecor.tsx
+++ frontend/src/components/ui/bouncing-balls/BouncingBallsDecor.tsx
@@ -1,8 +1,22 @@
 'use client';
 
-export function BouncingBallsDecor() {
+interface BouncingBallsDecorProps {
+  position?: 'bottom-right' | 'bottom-left' | 'top-right' | 'top-left';
+}
+
+export function BouncingBallsDecor({ position = 'bottom-right' }: BouncingBallsDecorProps) {
+  const positionStyles = {
+    'bottom-right': { bottom: '30px', right: '30px' },
+    'bottom-left': { bottom: '30px', left: '30px' },
+    'top-right': { top: '100px', right: '30px' },
+    'top-left': { top: '100px', left: '30px' },
+  };
+
   return (
-    <div className="bouncing-balls-decor">
+    <div 
+      className="bouncing-balls-decor"
+      style={positionStyles[position]}
+    >
       <div className="bouncing-ball ball-1" />
       <div className="bouncing-ball ball-2" />
       <div className="bouncing-ball ball-3" />
