44113e99dd3c23b7b4e4ca9790c2d8f788e3c8e7
phessophissy <adefesobi@yahoo.com>
2026-01-22 22:50:41 +0100

    theme(22/60): Create BouncingBallsDecor component

---
+++ frontend/src/components/ui/BouncingBallsDecor.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+export function BouncingBallsDecor() {
+  return (
+    <div className="bouncing-balls-decor">
+      <div className="bouncing-ball ball-1" />
+      <div className="bouncing-ball ball-2" />
+      <div className="bouncing-ball ball-3" />
+    </div>
+  );
+}
